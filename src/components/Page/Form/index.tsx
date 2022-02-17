42a9f3a1d95cf9e32ab1cf8e71b03f4797d8a2e7
tzyn <tzyn.wang@gmail.com>
2022-02-17 20:03:36 +0800

    [fix] Reset block status when page refresh

--- src/components/Page/Form/index.tsx
+++ src/components/Page/Form/index.tsx
@@ -1,9 +1,10 @@
-import React, { memo, useState } from 'react'
+import React, { memo, useState, useEffect } from 'react'
 import useBlock from '@Hooks/useBlock'
 
 function FormPage(): React.ReactElement {
   const [block, setBlock] = useState<boolean>(true)
   useBlock({ block })
+  useEffect(() => setBlock(true), [])
 
   return (
     <div>
@@ -11,7 +12,6 @@ function FormPage(): React.ReactElement {
       <br />
       <input type="text" id="fname" name="fname" value="John" required />
       <br />
-
       <label htmlFor="lname">Last name:</label>
       <br />
       <input type="text" id="lname" name="lname" value="Doe" required />
@@ -19,7 +19,10 @@ function FormPage(): React.ReactElement {
       <br />
       <button onClick={() => setBlock(false)}>submit</button>
       <br />
-      Block status: {block ? 'page is block, click the submit button to unblock' : 'page is NOT block'}
+      Block status:{' '}
+      {block
+        ? 'page is block, click the submit button to unblock'
+        : 'page is NOT block'}
     </div>
   )
 }
